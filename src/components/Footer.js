fef274dd8b9c2da8a6aab7ad48186c9832d5993f
Ayush-0312 <kumarayush3121511@gmail.com>
2025-07-22 23:53:29 +0530

    updated links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,31 +1,43 @@
 import React from "react";
-import { FaGithub } from "react-icons/fa6";
-import { RiNetflixFill } from "react-icons/ri";
-import { AVATAR, GITHUB, NETFLIX } from "../utils/utils/constants";
+import { FaGithub, FaLinkedin } from "react-icons/fa6";
+//import { RiNetflixFill } from "react-icons/ri";
+import { AVATAR, GITHUB, LINKEDIN, PORTFOLIO } from "../utils/utils/constants";
 
 const Footer = () => {
+  const handleAvatarClick = () => {
+    window.open(PORTFOLIO, "_blank", "noopener,noreferrer");
+  };
+
   return (
     <footer className="relative bottom-0 w-screen z-10 py-4 md:py-8 md:px-4 bg-black text-gray-400">
       <div className="container mx-auto flex justify-between items-center px-6">
         <p className="text-lg font-medium">NETFLIX-GPT</p>
         <div className="flex space-x-4">
-          <img className="w-6 rounded-xl" src={AVATAR} alt="avatar" />
+          <img
+            className="w-6 rounded-full cursor-pointer"
+            src={AVATAR}
+            alt="avatar"
+            onClick={handleAvatarClick}
+          />
           <a
             href={GITHUB}
             target="_blank"
             rel="noopener noreferrer"
             className="hover:text-gray-300"
           >
-            <FaGithub size={20} />
+            <FaGithub size={30} />
           </a>
-          <a
+          <a href={LINKEDIN} target="_blank" rel="noopener noreferrer">
+            <FaLinkedin size={30} />
+          </a>
+          {/* <a
             href={NETFLIX}
             target="_blank"
             rel="noopener noreferrer"
             className="hover:text-gray-300"
           >
             <RiNetflixFill size={20} />
-          </a>
+          </a> */}
           {/* <a
             href="https://google.com"
             target="_blank"
--- src/utils/utils/constants.js
+++ src/utils/utils/constants.js
@@ -29,4 +29,9 @@ export const AVATAR = "https://avatars.githubusercontent.com/u/175679849?v=4";
 
 export const GITHUB = "https://github.com/Ayush-0312";
 
+export const LINKEDIN =
+  "https://www.linkedin.com/in/ayush-gupta-41484b234?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app";
+
+export const PORTFOLIO = "https://www.theayushgupta.in/";  
+
 export const NETFLIX = "https://www.netflix.com/in/";
